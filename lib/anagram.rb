65b2119056094cc4f27c90388f5671f36af4ccd8
Nick Inman <nick@theoatmeal.com>
2018-12-06 06:28:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require"pry"
+
+class Anagram
+  attr_accessor :anagram
+  
+  
+  def initalize(anagram)
+    @anagram = anagram
+  end
+  
+  def match
+    
+    
+    
+  end
+  
+  
+  
+end
