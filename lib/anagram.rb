732be9467652d9851dc376425301864275f716f4
Nick Inman <nick@theoatmeal.com>
2018-12-06 06:55:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require"pry"
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+     @word = word
+  end
+  
+  def match(anagrams)
+    binding.pry
+    
+    #if word.chars == anagrams.each.chars then = true, else = false
+  end
+  
+end
+
